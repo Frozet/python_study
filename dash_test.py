19d4da04ba0e53fd33c0d4c86251f55ae7558b69
Frozet <frozetls@gmail.com>
2023-05-02 19:30:35 +0800

    add study file with dash

---
+++ dash_test.py
@@ -0,0 +1,40 @@
+from dash import Dash, html, dcc, callback, Output, Input
+import plotly.express as px
+import pandas as pd
+
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
+
+app = Dash(__name__)
+
+app.layout = html.Div([
+    html.H1(children='Title of Dash App', style={'textAlign':'center'}),
+    dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
+    dcc.Dropdown(['lifeExp', 'pop', 'gdpPercap'], 'pop', id = 'dropdown-column'),
+    dcc.Graph(id='graph-content'),
+    dcc.Dropdown(sorted(df.year.unique()), 2007, id='dropdown-year'),
+    html.Div(id='graph2')
+])
+
+@callback(
+    Output('graph-content', 'figure'),
+    Input('dropdown-selection', 'value'),
+    Input('dropdown-column', 'value'),
+    #prevent_initial_call = True
+)
+def draw_line(country, column):
+    dff = df[df.country==country]
+    return px.line(dff, x='year', y=column)
+@callback(
+    Output('graph2', 'children'),
+    Input('dropdown-column', 'value'),
+    Input('dropdown-year', 'value')
+)
+def draw_bar(column, year):
+    dff = df[df.year==year]
+    if column == 'lifeExp':
+        df_temp = dff.groupby('continent').mean()
+    else:
+        df_temp = dff.groupby('continent').sum()
+    return dcc.Graph(figure = px.bar(df_temp, x=df_temp.index, y=column), id='bar-content')
+if __name__ == '__main__':
+    app.run_server(debug=True, use_reloader = False)
